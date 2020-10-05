64a1e7474666f5f525e54d9ab4312d745c91709d
OofChairDev <66800643+OofChairDev@users.noreply.github.com>
2020-10-05 14:42:29 -0400

    rickroll :D

---
+++ FunCogs/mycog.py
@@ -0,0 +1,10 @@
+from redbot.core import commands
+
+class ClydeFun(commands.Cog):
+    """Random fun commands"""
+
+    @commands.command()
+    async def rickroll(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("https://giphy.com/gifs/rick-astley-Ju7l5y9osyymQ")
