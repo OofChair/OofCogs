d27eece36b188ba7642de3c2404de6bb3bbc6a73
OofChair <66800643+OofChair@users.noreply.github.com>
2020-11-11 11:14:18 -0500

    added jebait cmd

--- vari/mycog.py
+++ vari/mycog.py
@@ -8,3 +8,9 @@ class OEEvariable(commands.Cog):
         """Rickroll your friends!"""
         # Your code will go here
         await ctx.send("https://giphy.com/gifs/rick-astley-Ju7l5y9osyymQ")
+
+    @commands.command()
+    async def jebait(self, ctx):
+        """Rickroll your friends!"""
+        # Your code will go here
+        await ctx.send("https://media1.tenor.com/images/065a5ddbe6bd52171c59967167217287/tenor.gif?itemid=14527830")
