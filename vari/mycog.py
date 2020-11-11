db693d74a8512500ea318519f7d296e569a362f7
OofChair <66800643+OofChair@users.noreply.github.com>
2020-11-11 11:07:11 -0500

    new test cog

---
+++ vari/mycog.py
@@ -0,0 +1,10 @@
+from redbot.core import commands
+
+class OEE_variable(commands.Cog):
+    """Random fun commands"""
+
+    @commands.command()
+    async def rickroll(self, ctx):
+        """Rickroll your friends!"""
+        # Your code will go here
+        await ctx.send("https://giphy.com/gifs/rick-astley-Ju7l5y9osyymQ")
