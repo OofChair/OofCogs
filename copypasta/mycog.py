c4c685d46cba4f2ab9bb98e26edb4fad5089fe9f
OofChair <66800643+OofChair@users.noreply.github.com>
2020-11-11 12:12:49 -0500

    Update mycog.py

--- copypasta/mycog.py
+++ copypasta/mycog.py
@@ -9,6 +9,6 @@ class Copypasta(commands.Cog):
         # Your code will go here
         embed=discord.Embed(title="Hi! I'm Oofchair - Experimental Edition :D", description="undefined?", color=0xff0000)
         embed.set_thumbnail(url="https://s.yimg.com/ny/api/res/1.2/12UU2JphAsbxTTDca.7QFQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9MTA4MDtoPTcxNg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2019-11/7b5b5330-112b-11ea-a77f-7c019be7ecae")
-        embed.add_field(name="", value="", inline=False)
+        embed.add_field(name="ohhhh shoot", value="that's why it didnt work :facepalm:", inline=False)
         embed.set_footer(text="OofChair - Experimental Edition")
         await ctx.send(embed=embed)
