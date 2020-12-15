b61bc003066ce8b82fa7598e8e14363b2d40d19b
OofChair <66800643+OofChair@users.noreply.github.com>
2020-12-15 14:01:12 -0500

    add sra (WIP)

---
+++ some-random-api/sra.py
@@ -0,0 +1,17 @@
+import aiohttp
+from redbot.core import commands
+
+
+class Mycog(commands.Cog):
+    """My custom cog"""
+
+    @commands.command()
+    async def test(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        async def something():
+            async with aiohttp.ClientSession() as session:
+                async with session.get("https://some-random-api.ml/facts/dog") as request:
+                    response = await request.json()
+                    print(response)
+                    await something()
