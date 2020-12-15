28b2fd6d1a3b9b8ed670a763571a652c6cfbe4dd
OofChair <66800643+OofChair@users.noreply.github.com>
2020-12-15 15:08:13 -0500

    time to repeat this 10 more times

--- sra/sra.py
+++ sra/sra.py
@@ -12,5 +12,6 @@ class sra(commands.Cog):
             async with session.get("https://some-random-api.ml/facts/dog") as request:
                 response = await request.json()
                 embed = discord.Embed(colour=await ctx.embed_colour())
+                embed.set_thumbnail(url='https://thetrendler.com/wp-content/uploads/2016/08/10tb-dogsperm01-superJumbo.jpg')
                 embed.add_field(name='Here\'s a random dog fact!',value=response['fact'])
                 await ctx.send(embed=embed)
