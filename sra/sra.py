5191349d12b88bd5da337c9e5ceb40b69da55833
OofChair <66800643+OofChair@users.noreply.github.com>
2020-12-15 14:50:46 -0500

    pls work :heart:

--- sra/sra.py
+++ sra/sra.py
@@ -1,17 +1,13 @@
 import aiohttp
 from redbot.core import commands
 
-
 class sra(commands.Cog):
     """API requests from Some Random API"""
-
     @commands.command()
     async def dogfact(self, ctx):
         """Gets random dog fact"""
         # Your code will go here
-        async def something():
             async with aiohttp.ClientSession() as session:
                 async with session.get("https://some-random-api.ml/facts/dog") as request:
                     response = await request.json()
-                    print(response)
-                    await something()
+             await ctx.send(response['fact'])
