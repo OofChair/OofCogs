01ed83c2f2c8c617d31b0ba5cceae03a9dfe7b20
OofChair <66800643+OofChair@users.noreply.github.com>
2020-12-16 08:16:13 -0500

    subcommands :D

--- sra/sra.py
+++ sra/sra.py
@@ -4,10 +4,12 @@ from redbot.core import commands
 
 class sra(commands.Cog):
     """API requests from Some Random API"""
-    @commands.command()
-    async def dogfact(self, ctx):
-        """Gets random dog fact"""
-        # Your code will go here
+    @commands.group()
+    async def fact(ctx):
+        """Gets fact from which animal you choose"""
+
+    @fact.command()
+    async def dog(ctx, self)
         async with aiohttp.ClientSession() as session:
             async with session.get("https://some-random-api.ml/facts/dog") as request:
                 response = await request.json()
