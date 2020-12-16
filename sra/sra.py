b66d63a4c8dd9b898f0a44e174048d26f6e42d79
OofChair <66800643+OofChair@users.noreply.github.com>
2020-12-16 08:40:28 -0500

    colour

--- sra/sra.py
+++ sra/sra.py
@@ -14,7 +14,7 @@ class sra(commands.Cog):
         async with aiohttp.ClientSession() as session:
             async with session.get("https://some-random-api.ml/facts/dog") as request:
                 response = await request.json()
-                embed = discord.Embed(colour=await ctx.embed_colour())
+                embed = discord.Embed(color=(await ctx.embed_colour()))
                 embed.set_image(url='https://thetrendler.com/wp-content/uploads/2016/08/10tb-dogsperm01-superJumbo.jpg')
                 embed.add_field(name='Here\'s a random dog fact!',value=response['fact'])
                 await ctx.send(embed=embed)
