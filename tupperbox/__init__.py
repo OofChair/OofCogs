0b735b5754cc1c62c9c180b4718c7748a8ad2993
OofChair <66800643+OofChair@users.noreply.github.com>
2020-12-18 13:30:26 -0500

    add mm perm

---
+++ tupperbox/__init__.py
@@ -0,0 +1,5 @@
+from .tupperbox import Tupperbox
+
+
+def setup(bot):
+  bot.add_cog(Tupperbox())
---
+++ tupperbox/tupperbox.py
@@ -0,0 +1,16 @@
+from redbot.core import commands
+import discord
+
+class Tupperbox(commands.Cog):
+    """Random fun commands"""
+
+    @commands.command()
+    async def tupcreate(self, ctx):
+        """Create your own tupper character!"""
+        # Your code will go here
+
+    @commands.command()
+    async def pog(self, ctx):
+        """:PogChamp:"""
+        # Your code will go here
+        await ctx.send("https://media1.tenor.com/images/065a5ddbe6bd52171c59967167217287/tenor.gif?itemid=14527830")
