b2afd248fc2e4be74feb5e2a0df153b72c426f44
OofChair <oofchairr@gmail.com>
2020-12-30 09:43:42 -0500

    nope

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -7,4 +7,4 @@ class Mycog(commands.Cog):
     async def test(self, ctx):
         """This does stuff!"""
         # Your code will go here
-            await ctx.send("helo!")
+        await ctx.send("helo!")
