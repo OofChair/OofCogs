80e5003ebb4b0466a8192a103898055027998b2c
OofChair <oofchairr@gmail.com>
2021-01-20 12:40:49 -0500

    start randmeme

--- randmeme/randmeme.py
+++ randmeme/randmeme.py
@@ -1,5 +1,5 @@
 from typing import Literal
-
+import aiohttp
 import discord
 from redbot.core import commands
 from redbot.core.bot import Red
@@ -24,3 +24,16 @@ class RandMeme(commands.Cog):
     async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int) -> None:
         # TODO: Replace this with the proper end user data removal handling.
         super().red_delete_data_for_user(requester=requester, user_id=user_id)
+
+
+# actual code :Kappa:
+    @commands.command()
+    async def randmeme(self, ctx):
+        """Get a random dog fact"""
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://imageapi.fionn.live/reddit/memes") as request:
+                response = await request.json()
+                embed = discord.Embed(color=(await ctx.embed_colour()))
+                embed.set_image(f"{img}")
+                embed.add_field(name=f'{title}',value=response[f'u/{author}'])
+                await ctx.send(embed=embed)
