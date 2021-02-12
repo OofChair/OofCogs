e7c4e313b6d4825e8df6bb1497b8fca950e90f26
OofChair <oofchairr@gmail.com>
2021-02-12 10:23:43 -0500

    this?

--- redditpic/redditpic.py
+++ redditpic/redditpic.py
@@ -56,13 +56,13 @@ class RedditPic(commands.Cog):
     async def subr(self, ctx, subreddit):
         """Get a random picture from a subreddit \n\n If an error occurs, please wait a few seconds, then try again."""
         try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                f"https://imageapi.fionn.live/reddit/{subreddit}"
-            ) as request:
-                response = await request.json()
-                except {response['err']}:
-                    await ctx.send("test")
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                    f"https://imageapi.fionn.live/reddit/{subreddit}"
+                    ) as request:
+                    response = await request.json()
+                        except {response['err']}:
+                            await ctx.send("test")
                 embed = discord.Embed(color=(await ctx.embed_colour()))
 
                 embed.set_image(url=response["img"])
