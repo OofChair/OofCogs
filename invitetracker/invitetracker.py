669346c458b443c10042a696c900de7cb0278e1f
OofChair <oofchairr@gmail.com>
2021-10-22 20:23:06 -0400

    [Repo] Changes to comply with CC review (#26)

--- invitetracker/invitetracker.py
+++ invitetracker/invitetracker.py
@@ -5,6 +5,7 @@ from redbot.core import commands
 from redbot.core.bot import Red
 from redbot.core.config import Config
 from collections import defaultdict
+from redbot.core.errors import CogLoadError
 
 RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
 
@@ -43,16 +44,18 @@ class InviteTracker(commands.Cog):
         self, *, requester: RequestType, user_id: int
     ) -> None:
         # TODO: Replace this with the proper end user data removal handling.
-        super().red_delete_data_for_user(requester=requester, user_id=user_id)
+        return
 
-    async def load(self):
-        for guild in self.bot.guilds:
+    async def load(self, ctx):
+        if ctx.me.guild_permissions.manage_guild == True:
             try:
                 self.invites[guild.id] = await guild.invites()
             except discord.Forbidden:
                 pass
             except Exception as e:
                 print(e, flush=True)
+        else:
+            raise CogLoadError("")
 
     def find_invite_by_code(self, inv_list, code):
         for inv in inv_list:
@@ -61,7 +64,7 @@ class InviteTracker(commands.Cog):
 
     @commands.group(aliases=["invset"])
     @commands.admin()
-    @commands.bot_in_a_guild()
+    @commands.guild_only()
     async def invitetrackerset(self, ctx):
         """Invite tracker settings
 
@@ -80,9 +83,14 @@ class InviteTracker(commands.Cog):
         Arguments:
         `channel`: Select the channel for the invite logging to be sent to
         """
-        async with ctx.typing():
-            await self.config.guild(ctx.guild).channel.set(channel.id)
+        if ctx.channel.permissions_for(channel).send_messages == True:
+            async with ctx.typing():
+                await self.config.guild(ctx.guild).channel.set(channel.id)
             await ctx.send(f"The log channel has been set to {channel.mention}")
+        else:
+            await ctx.send(
+                "I can't send messages in that channel! Please give me perms and retry this command."
+            )
 
     @invitetrackerset.command()
     async def enable(self, ctx, yes_or_no: bool):
@@ -93,10 +101,10 @@ class InviteTracker(commands.Cog):
         """
         async with ctx.typing():
             await self.config.guild(ctx.guild).enabled.set(yes_or_no)
-            if yes_or_no:
-                await ctx.send("Invite tracking has been turned on for this guild.")
-            else:
-                await ctx.send("Invite tracking has been turned off for this guild.")
+        if yes_or_no:
+            await ctx.send("Invite tracking has been turned on for this guild.")
+        else:
+            await ctx.send("Invite tracking has been turned off for this guild.")
 
     @invitetrackerset.command()
     async def leaveenable(self, ctx, yes_or_no: bool):
@@ -107,14 +115,10 @@ class InviteTracker(commands.Cog):
         """
         async with ctx.typing():
             await self.config.guild(ctx.guild).leaveenabled.set(yes_or_no)
-            if yes_or_no:
-                await ctx.send(
-                    "Leave invite tracking has been turned on for this guild."
-                )
-            else:
-                await ctx.send(
-                    "Leave invite tracking has been turned off for this guild."
-                )
+        if yes_or_no:
+            await ctx.send("Leave invite tracking has been turned on for this guild.")
+        else:
+            await ctx.send("Leave invite tracking has been turned off for this guild.")
 
     @invitetrackerset.command()
     async def joinenable(self, ctx, yes_or_no: bool):
@@ -125,36 +129,37 @@ class InviteTracker(commands.Cog):
         """
         async with ctx.typing():
             await self.config.guild(ctx.guild).joinenabled.set(yes_or_no)
-            if yes_or_no is True:
-                await ctx.send(
-                    "Join invite tracking has been turned on for this guild."
-                )
-            else:
-                await ctx.send(
-                    "Join invite tracking has been turned off for this guild."
-                )
+        if yes_or_no is True:
+            await ctx.send("Join invite tracking has been turned on for this guild.")
+        else:
+            await ctx.send("Join invite tracking has been turned off for this guild.")
 
     @commands.command(aliases=["userinvites"])
     async def invitesforuser(self, ctx, user: discord.Member = None):
         """See how many times a user's invites have been used"""
-        async with ctx.typing():
-            if user is None:
-                user = ctx.author
-            total_invites = 0
-            for i in await ctx.guild.invites():
-                if i.inviter == user:
-                    total_invites += i.uses
-            embed = discord.Embed(title="📫 Invite counter")
-            embed.add_field(
-                name=f"​​​​​Invites for {user.name}#{user.discriminator}",
-                value=f"{total_invites} times!",
-            )
+        if ctx.channel.permissions_for(ctx.me).manage_guild == True:
+            async with ctx.typing():
+                if user is None:
+                    user = ctx.author
+                total_invites = 0
+                for i in await ctx.guild.invites():
+                    if i.inviter == user:
+                        total_invites += i.uses
+                embed = discord.Embed(title="📫 Invite counter")
+                embed.add_field(
+                    name=f"​​​​​Invites for {user.name}#{user.discriminator}",
+                    value=f"{total_invites} times!",
+                )
             await ctx.send(embed=embed)
+        else:
+            await ctx.send(
+                "I can't access invites. Please make sure I have the right permissions and try again."
+            )
 
     # Invite tracking
 
     @commands.Cog.listener()
-    async def on_member_join(self, member: discord.Member) -> None:
+    async def on_member_join(self, ctx, member: discord.Member) -> None:
         """On member listener for new users"""
         logs_channel = await self.config.guild(member.guild).channel()
         logs = member.guild.get_channel(logs_channel)
@@ -181,11 +186,23 @@ class InviteTracker(commands.Cog):
         if (
             self.config.guild(member.guild).enabled
             and self.config.guild(member.guild).joinenabled
+            and ctx.channel.permissions_for(ctx.me).manage_guild() == True
         ):
             await logs.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                description="Just joined the server", color=0x03D692, title=" "
+            )
+            embed.set_author(name=str(member), icon_url=member.avatar_url)
+            embed.set_footer(text="ID: " + str(member.id))
+            embed.add_field(
+                name="Couldn't find invite!",
+                value=f"I couldn't find the invite that this user used. ",
+                inline=False,
+            )
 
     @commands.Cog.listener()
-    async def on_member_remove(self, member: discord.Member) -> None:
+    async def on_member_remove(self, ctx, member: discord.Member) -> None:
         """On member listener for users leaving"""
         logs_channel = await self.config.guild(member.guild).channel()
         logs = member.guild.get_channel(logs_channel)
@@ -213,5 +230,17 @@ class InviteTracker(commands.Cog):
         if (
             self.config.guild(member.guild).enabled
             and self.config.guild(member.guild).joinenabled
+            and ctx.channel.permissions_for(ctx.me).manage_guild() == True
         ):
             await logs.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                description="Just joined the server", color=0x03D692, title=" "
+            )
+            embed.set_author(name=str(member), icon_url=member.avatar_url)
+            embed.set_footer(text="ID: " + str(member.id))
+            embed.add_field(
+                name="Couldn't find invite!",
+                value=f"I couldn't find the invite that this user used. ",
+                inline=False,
+            )
